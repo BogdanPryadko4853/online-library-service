22f9b105ff3d01e7fe240aa6d97591e96810e0f7
Bogdan Pryadko <bogdanpryadko1@gmail.com>
2025-02-27 15:20:50 +0300

    add observers in userRepository and loanRepository

--- src/repositories/LoanRepository.cpp
+++ src/repositories/LoanRepository.cpp
@@ -1,3 +1,15 @@
 #include "../../include/repositories/LoanRepository.h"
 
 std::shared_ptr<LoanRepository> LoanRepository::instance = nullptr;
+
+std::shared_ptr<Loan> LoanRepository::findById(int id) const {
+    auto loan = Repository::findById(id);
+    notifyObservers(&"Getting loan with id - "[id]);
+    return loan;
+}
+
+std::vector<std::shared_ptr<Loan>> LoanRepository::getAll() const {
+    auto loans = Repository::getAll();
+    notifyObservers("Getting all loans");
+    return loans;
+}
--- src/repositories/UserRepository.cpp
+++ src/repositories/UserRepository.cpp
@@ -1,3 +1,15 @@
 #include "../../include/repositories/UserRepository.h"
 
 std::shared_ptr<UserRepository> UserRepository::instance = nullptr;
+
+std::shared_ptr<User> UserRepository::findById(int id) const {
+   auto user =  Repository::findById(id);
+    notifyObservers(&"Getting user with id - "[id]);
+    return user;
+}
+
+std::vector<std::shared_ptr<User>> UserRepository::getAll() const {
+    auto users = Repository::getAll();
+    notifyObservers("Getting all users");
+    return users;
+}
